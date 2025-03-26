872408f786fa396cc5e14bf997453027e2d931d9
LachanaVasiliki <56480887+LachanaVasiliki@users.noreply.github.com>
2025-03-26 19:37:56 +0200

    Add files via upload

---
+++ om.java
@@ -0,0 +1,12 @@
+package tryjava3rdomadiki;
+
+
+
+public class Tryjava3rdomadiki {
+	public static void main(String args[]){
+			BoardDisplay game = new BoardDisplay(15, 8);
+			game.buildGrid();
+		}
+	
+	
+}
